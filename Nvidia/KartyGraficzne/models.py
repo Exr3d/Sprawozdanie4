794b0a5033f2d217565751ecbc9fe83ad1cbfafc
Exred <tymonwoj57@gmail.com>
2022-01-26 20:42:21 +0100

    created serializer + model Class + views

--- Nvidia/KartyGraficzne/models.py
+++ Nvidia/KartyGraficzne/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Karty_Graficzne(models.Model):
+    Marka = models.CharField(max_length=255)
+    Model = models.CharField(max_length=255)
+    VRAM = models.IntegerField(max_length=5)
+    avgCena = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.Marka + " " + self.Model + " "
